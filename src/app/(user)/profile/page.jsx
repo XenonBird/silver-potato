96ce55af002e2d697129a9ae657ed7ac46edee76
XenonBird <bird.xenon@gmail.com>
2023-12-02 17:26:26 +0530

    minor fix

--- src/app/(user)/profile/page.jsx
+++ src/app/(user)/profile/page.jsx
@@ -16,7 +16,7 @@ const ProfileData = () => {
   return (
     <main className="grow overflow-y-scroll">
       <div className="mx-auto max-w-md flex flex-col gap-4 p-4">
-        <pre>{JSON.stringify(decodedData.data, null, 2)}</pre>
+        // <pre>{JSON.stringify(decodedData.data, null, 2)}</pre>
         {/* User Profile */}
         <section className="flex flex-col gap-4 w-full border rounded-xl p-4">
           <h2 className="font-semibold text-xl text-center">
@@ -47,7 +47,6 @@ const ProfileData = () => {
           </div>
           {/* Add more profile details as needed */}
         </section>
-
         <section className="flex flex-col gap-4 w-full border rounded-xl p-4">
           <Link href="/auth/logout" className="btn btn-primary btn-outline">
             Log out
